1d6c73b0a7bc7dac4a2f1e3c73769b1a98cbbfce
Daniel Barden <daniel.barden@gmail.com>
2011-09-20 09:34:36 -0300

    Initial Commit

---
+++ locationSample2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  locationSample2
+//
+//  Created by Daniel Barden on 9/20/11.
+//  Copyright (c) 2011 None. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
